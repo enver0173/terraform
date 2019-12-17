3463fe69c2005093d13325452a9f234c9d359ade
enver0173 <--list>
2019-12-16 20:33:43 -0600

    removed doubled cot

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   ami           = "ami-0b2d8d1abb76a53d8" 
   instance_type = "t2.micro" 
   associate_public_ip_address = "true"
-  key_name = "aws_key_pair.deployer.key_name"
+  key_name = aws_key_pair.deployer.key_name
  
 
 tags = { 
