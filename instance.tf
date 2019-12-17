6a75dc918c58f97a5b6eb70e6342dca286b256f1
enver0173 <--list>
2019-12-16 20:22:59 -0600

    added ip

--- instance.tf
+++ instance.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" { 
-  ami           = "ami-0c5204531f799e0c6" 
+  ami           = "ami-08d489468314a58df" 
   instance_type = "t2.micro" 
+  associate_public_ip_address = "true"
  
 
 tags = { 
