e55d08b4ff4e0bd38096d8db11bfc7c5184c4ca5
enver0173 <--list>
2019-12-16 20:25:34 -0600

    added w

--- instance.tf
+++ instance.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "web" { 
-  ami           = "ami-08d489468314a58df" 
+  ami           = "ami-0b2d8d1abb76a53d8" 
   instance_type = "t2.micro" 
   associate_public_ip_address = "true"
+  key_name = "${aws_key_pair.deployer}"
  
 
 tags = { 
