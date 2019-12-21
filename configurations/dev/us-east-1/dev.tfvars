2f43a9114bdd016280d2af0bf785f2e06c6dcadb
enver0173 <--list>
2019-12-21 11:29:14 -0600

    added

---
+++ configurations/dev/us-east-1/dev.tfvars
@@ -0,0 +1,1 @@
+ami           = "ami-0b2d8d1abb76a53d8" 
--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" { 
   count = 4
-  ami           = "ami-0b2d8d1abb76a53d8" 
+  ami           = var.ami 
   instance_type = "t2.micro" 
   associate_public_ip_address = "true"
   key_name = aws_key_pair.deployer.key_name
