36b36fdec7c3278dd5438c50c3d12802b72f8b32
enver0173 <--list>
2019-12-21 12:36:56 -0600

    added

--- variables.tf
+++ variables.tf
@@ -3,7 +3,10 @@ variable "instance_type" {}
 variable "count_instance" {
   
 }
-variable " associate_public_ip_address" 
+variable " associate_public_ip_address" {
+    
+}
+
 variable "user" {
   
 }
