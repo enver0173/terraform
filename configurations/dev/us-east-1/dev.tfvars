14bb3752c1ce1213da9aacbcf6d0ebf411bf88f0
enver0173 <--list>
2019-12-21 11:59:21 -0600

    added

--- configurations/dev/us-east-1/dev.tfvars
+++ configurations/dev/us-east-1/dev.tfvars
@@ -1,3 +1,4 @@
 ami           = "ami-0b2d8d1abb76a53d8" 
 instance_type = "t2.micro" 
 count_instance         = 4
+associate_public_ip_address = "true"
--- instance.tf
+++ instance.tf
@@ -2,13 +2,16 @@ resource "aws_instance" "web" {
   count = var.count_instance
   ami           = var.ami 
   instance_type = "var.instance_type" 
-  associate_public_ip_address = "true"
+  associate_public_ip_address = var.associate_public_ip_address
   key_name = aws_key_pair.deployer.key_name
   security_groups = ["allow_ssh1"] 
 
 
 
    user_data = file("userdata_file")
+   lifecycle = {
+     prevent_destroy = false
+   }
  
 
    tags = { 
