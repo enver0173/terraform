7cec4176eea5b1bf3ca9f4eff6099a3101caa473
enver0173 <--list>
2020-01-04 12:36:11 -0600

    added

--- ENVIRONMENT/variables.tf
+++ ENVIRONMENT/variables.tf
@@ -8,3 +8,14 @@ variable "key_name" {
 variable "instance_type" {
   
 }
+variable "environment" {} 
+
+variable "s3_bucket" {} 
+
+variable "s3_folder_project" {} 
+
+variable "s3_folder_region" {} 
+
+variable "s3_folder_type" {} 
+
+variable "s3_tfstate_file" {} 
