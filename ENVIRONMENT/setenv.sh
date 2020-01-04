117066bc72222d9782c4039aa9433df0ddf21733
enver0173 <--list>
2020-01-04 12:55:39 -0600

    ase

--- ENVIRONMENT/setenv.sh
+++ ENVIRONMENT/setenv.sh
@@ -137,21 +137,13 @@ then
    echo "setenv: 's3_tfstate_file' variable not set in configuration file." 
 
    echo "e.g. s3_tfstate_file=\"infrastructure.tfstate\"" 
-
 return 1 
-
 fi 
-
 cat << EOF > "$DIR/backend.tf" 
-
 terraform { 
-
 backend "s3" {
-
 bucket = "${S3BUCKET}" 
-
 key = "${S3BUCKETPROJ}/${S3BUCKETREGION}/${S3BUCKETTYPE}/${ENVIRONMENT}/${S3TFSTATEFILE}" 
-
 region = "${S3BUCKETREGION}" 
   } 
 } 
