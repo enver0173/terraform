a833736c6698ac4839a4f32ca363cceceb6792de
enver0173 <--list>
2020-01-04 10:59:47 -0600

    ade

--- configurations/IAM/backend.tf
+++ configurations/IAM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend “s3” {
-    bucket = “terrraform-us-chyngyz” #backend-iaac-chyngyz
+    bucket = “terraformenver123” #backend-iaac-chyngyz
     key    = “aws/iam/iam_state”
     region = “us-east-1"
   }
