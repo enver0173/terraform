9ff0c0e23629d125acad16f0a9da16e70a54b47e
enver0173 <--list>
2020-01-08 19:59:59 -0600

    ase

--- data_source/tower.tf
+++ data_source/tower.tf
@@ -12,22 +12,22 @@ data "aws_ami" "ubuntu" {
 }
 output "centos" {
     value = data.aws_ami.centos.id
-} 
+}
 resource "aws_key_pair" "towerkey" { 
   key_name   = "towerkey" 
   public_key = file("~/.ssh/id_rsa.pub") 
 } 
 resource "aws_instance" "tower" {
-  ami           = "data.aws_ami.centos.id
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.towerkey.key_name
-  provisioner "remote-exec" {
+    ami           = "data.aws_ami.centos.id
+    instance_type = "t2.micro"
+    key_name      = aws_key_pair.towerkey.key_name
+    provisioner "remote-exec" {
       connection {
-      host = self.public_ip
-      type = "ssh"
-      user = "centos"
-      private_key = file("~/.ssh/id_rsa")
-      }
+          host = self.public_ip
+          type = "ssh"
+          user = "centos"
+          private_key = file("~/.ssh/id_rsa")
+          }
     inline = [
         "sudo yum install -y epel-release",
         ]
