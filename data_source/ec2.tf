6821bff61892cc0727ba63d1afe900c076374927
enver0173 <--list>
2020-01-08 19:00:34 -0600

    aws

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -2,9 +2,13 @@ provider "aws" {
     region = "us-east-2" 
 } 
 data "aws_ami" "ubuntu" {
-    most_recent = true 
-    owners = ["099720109477"] 
-} 
+    filter {
+        name = "root-device-type"
+        values = ["ebs"]
+        }
+        most_recent = true
+        owners = ["099720109477"]
+        }
 output "ami" {
     value = "${data.aws_ami.ubuntu.id}" 
 } 
